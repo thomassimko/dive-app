3f2d3feb586edbb774da5c24f071c1b9885d5195
Thomas Simko <tjsimko@gmail.com>
2018-02-23 10:30:44 -0800

    Initial Commit

---
+++ Final Dive AppTests/Final_Dive_AppTests.swift
@@ -0,0 +1,36 @@
+//
+//  Final_Dive_AppTests.swift
+//  Final Dive AppTests
+//
+//  Created by Thomas Simko on 2/23/18.
+//  Copyright © 2018 Thomas Simko. All rights reserved.
+//
+
+import XCTest
+@testable import Final_Dive_App
+
+class Final_Dive_AppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
